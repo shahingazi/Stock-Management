a40b4b9780afe2ba5ae92dca4a5d4ebc2c3de678
shahingazi <shahinkgc84@gmail.com>
2018-08-22 15:07:27 +0200

    add product table

---
+++ StockManagement/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StockManagement.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BarCode { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CratedAt { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
